1599d3bdcb62b2949b648771fd160ea81adf27aa
AgustinPalmaM <palma.agustin7@gmail.com>
2022-09-14 16:58:34 -0300

    Create CRUD for Image Model

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,46 @@
 class ImagesController < ApplicationController
-
+  before_action :set_image, only: %i[show edit update destroy] 
   def index
+    @images = Image.all.sort
+  end
+
+  def show
+  end
   
+  def new
+    @image = Image.new
+  end
+  
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to @image
+    end
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+    if @image.update(image_params)
+      redirect_to images_path
+    end
+  end
+
+  def destroy
+    
+    @image.destroy
+    redirect_to images_path
+  end
+  private
+  
+  def image_params
+    params.require(:image).permit(:description)
+  end
+  
+  def set_image
+    @image = Image.find(params[:id])
   end
 end
