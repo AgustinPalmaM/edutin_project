c6e13becbac31678f7788574941b65692ccbf8ea
AgustinPalmaM <palma.agustin7@gmail.com>
2022-09-14 18:33:19 -0300

    Add bootstrap, update appplicatios.css to application.scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+(1..100).each do |i|
+  Image.create(id: i, description: "Imagen Numero #{i}", likes_counter: rand(1000))
+end
